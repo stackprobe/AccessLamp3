dcf76d5bf2ed439312eb54210e5aa36e2f595d8f
stackprobe <stackprobe@gmail.com>
2025-12-27 10:49:09 +0900

    add:0 mod:0 del:26 mv:156 +0 -92 2025/12/27 10:49:09 (+0:04:18)

--- AccessLamp_D/t20210403_AccessLamp_D/Upload.bat
+++
@@ -1,7 +0,0 @@
-CALL qq
-cx **
-
-CALL Release.bat /B
-
-C:\Factory\Petra\UpdatingCopy.exe out C:\be\Web\DocRoot\wb\*
-C:\Factory\Petra\RunOnBatch.exe C:\be\Web Upload.bat
--- AccessLamp_D/t20210405_AccessLamp_D/Release.bat
+++
@@ -1,7 +0,0 @@
-C:\Factory\Tools\RDMD.exe /RC out
-
-COPY /B AccessLamp\AccessLamp\bin\Release\AccessLamp.exe out\AccessLamp_D.exe
-
-C:\Factory\Tools\xcp.exe doc out
-
-C:\Factory\SubTools\zip.exe /O out AccessLamp_D
